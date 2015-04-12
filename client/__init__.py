dda45d20dae48f78bbf5a6c115f1466e502907ad
Marshall Davis <toothlessrebel@gmail.com>
2015-04-12 03:24:37 -0400

    Refactors startup and activate preferences menu
    Refactors the way the application starts.
    Adds window decorations.
    Allows prefences to be opened.

--- client/__init__.py
+++ client/__init__.py
@@ -1,1 +1,9 @@
 __author__ = 'ToothlessRebel'
+__all__ = [
+    'Client',
+    'ClientUI',
+    'Preferences'
+]
+from client.client import Client
+from client.client import ClientUI
+from client.client import Preferences
--- client/client.py
+++ client/client.py
@@ -2,34 +2,37 @@
 __author__ = 'ToothlessRebel'
 import tkinter as tk
 from tkinter.font import Font
-from socket import *
-from queue import *
-from time import *
-from threading import *
-from pprint import pprint
+from socket import socket
+from queue import Queue
+from time import sleep
+from threading import Thread
 
 
-class ClientUI:
+class ClientUI(tk.Frame):
     def __init__(self, master, queue, send_command):
+        super(ClientUI, self).__init__()
         self.queue = queue
         self.send_command = send_command
         self.master = master
+
         menu_bar = tk.Menu(master)
         menu_file = tk.Menu(menu_bar, tearoff=0)
         menu_file.add_command(label="Preferences", command=self.show_preferences)
         menu_file.add_command(label="Quit", command=master.destroy)
         menu_bar.add_cascade(label="Client", menu=menu_file)
         self.master.config(menu=menu_bar)
+
         self.master.grid()
         self.create_widgets()
+
         self.output_panel = master.children['output']
         italic_font = Font(self.output_panel, self.output_panel.cget("font"))
         italic_font.configure(slant='italic')
         self.output_panel.tag_configure("italic", font=italic_font)
 
-    def draw_output(self, line):
+    def draw_output(self, line, tags=None):
         self.output_panel.configure(state="normal")
-        self.output_panel.insert(tk.END, (line + "\n"))
+        self.output_panel.insert(tk.END, (line + "\n"), tags)
         self.output_panel.configure(state="disabled")
 
     def create_widgets(self):
@@ -45,10 +48,12 @@ class ClientUI:
         text = user_input.widget.get()
         user_input.widget.delete(0, tk.END)
         self.send_command(text)
+        if True:
+            self.draw_output(text, 'italic')
 
-    @staticmethod
-    def show_preferences():
-        pass
+    def show_preferences(self):
+        prefs = Preferences(self.master)
+        prefs.grid()
 
 
 class Client:
@@ -77,6 +82,8 @@ class Client:
                 self.ui.draw_output(line)
 
 
-root = tk.Tk()
-client = Client(root)
-root.mainloop()
+class Preferences(tk.Frame):
+    def __init__(self, master):
+        super(Preferences, self).__init__()
+        prefs = tk.Toplevel(self)
+        prefs.wm_title("Preferences")
