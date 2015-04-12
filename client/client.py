6bbc9ba388dc70a23890d0b84d7245ecf52671c7
Marshall Davis <toothlessrebel@gmail.com>
2015-04-12 00:08:01 -0400

    Adds working communication.

--- client/client.py
+++ client/client.py
@@ -6,8 +6,7 @@ from socket import *
 from queue import *
 from time import *
 from threading import *
-# import irc
-# from pprint import pprint
+from pprint import pprint
 
 
 class ClientUI:
@@ -15,21 +14,23 @@ class ClientUI:
         # super(ClientUI, self).__init__()
         self.queue = queue
         self.send_command = send_command
+        self.master = master
         menu_bar = tk.Menu(master)
         menu_file = tk.Menu(menu_bar, tearoff=0)
         menu_file.add_command(label="Preferences", command=self.show_preferences)
         menu_file.add_command(label="Quit", command=master.destroy)
         menu_bar.add_cascade(label="Client", menu=menu_file)
-        master.config(menu=menu_bar)
-        master.grid()
+        self.master.config(menu=menu_bar)
+        self.master.grid()
         self.create_widgets()
         self.output_panel = master.children['output']
         italic_font = Font(self.output_panel, self.output_panel.cget("font"))
         italic_font.configure(slant='italic')
         self.output_panel.tag_configure("italic", font=italic_font)
-        while True:
-            self.draw_output()
-            sleep(0)
+
+        # while True:
+        #     self.draw_output()
+        #     sleep(0)
         # pprint(self.output_panel)
 
     # def get_output(self):
@@ -43,37 +44,28 @@ class ClientUI:
     #
     #         self.draw_output()
 
-    def draw_output(self):
-        while self.queue.qsize():
-            try:
-                self.output_panel.configure(state="normal")
-                self.output_panel.insert(tk.END, (self.queue.get(0) + "\n"))
-                self.output_panel.configure(state="disabled")
-            except Queue.empty(self.queue):
-                pass
+    def draw_output(self, line):
+        self.output_panel.configure(state="normal")
+        self.output_panel.insert(tk.END, (line + "\n"))
+        self.output_panel.configure(state="disabled")
 
     def create_widgets(self):
-        output = tk.Text(self, state=tk.DISABLED, name="output")
+        output = tk.Text(self.master, state=tk.DISABLED, name="output")
         output.grid(row=0)
 
-        input_area = tk.Entry(self, name="input")
-        input_area.bind("<Return>", self.send_command)
+        input_area = tk.Entry(self.master, name="input")
+        input_area.bind("<Return>", self.parse_input)
         input_area.focus()
         input_area.grid(row=1, sticky=tk.W+tk.E)
+
+    def parse_input(self, user_input):
+        text = user_input.widget.get()
+        user_input.widget.delete(0, tk.END)
+        self.send_command(text)
 
     @staticmethod
     def show_preferences():
         pass
-
-    # def send_command(self, event):
-    #     command = event.widget.get()
-    #     print("Sending command " + command)
-    #     # pprint(vars(event.widget.master.children['output']))
-    #     self.socket.send(bytes(command + "\r\n", 'UTF-8'))
-    #     if True:
-    #         self.queue.put(command)
-    #
-    #     event.widget.delete(0, tk.END)
 
 
 class Client:
@@ -83,19 +75,24 @@ class Client:
         self.queue = Queue()
         self.ui = ClientUI(master, self.queue, self.send)
         self.socket = socket()
-        socket.connect(self.socket, ("tec.skotos.net", 6730))
-
         self.listener = Thread(target=self.listen)
+        self.listener.start()
 
     def send(self, command):
-        self.socket.send(bytes(command, encoding='utf8'))
+        total_successful = 0
+        while total_successful < command.__len__():
+            successful = self.socket.send(bytes(command[total_successful:] + "\r\n", "UTF-8"))
+            if successful == 0:
+                raise RuntimeError("Unable to send message.")
+            total_successful = total_successful + successful
 
     def listen(self):
+        socket.connect(self.socket, ("tec.skotos.net", 6730))
         while True:
             sleep(0)
-            buffer = str(self.socket.recv(406), encoding='utf8').split("\r\n")
+            buffer = str(self.socket.recv(4096), encoding='utf8').split("\r\n")
             for line in buffer:
-                self.queue.put(line)
+                self.ui.draw_output(line)
 
 
 root = tk.Tk()
