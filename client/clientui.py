12ecd1f8c6f8720297e249780e5cf7c6d9ee2b21
Marshall Davis <toothlessrebel@gmail.com>
2015-04-13 01:43:37 -0400

    Adding protocol for WM_WINDOW_DESTROY

--- client/clientui.py
+++ client/clientui.py
@@ -5,8 +5,9 @@ import tkinter as tk
 
 
 class ClientUI(tk.Frame):
-    def __init__(self, master, queue, send_command):
+    def __init__(self, master, client, queue, send_command):
         super(ClientUI, self).__init__()
+        self.client = client
         self.queue = queue
         self.send_command = send_command
         self.master = master
@@ -28,11 +29,21 @@ class ClientUI(tk.Frame):
 
     def draw_output(self, line, tags=None):
         self.output_panel.configure(state="normal")
+        scroll_position = self.output_panel.scrollbar.get()
         self.output_panel.insert(tk.END, (line + "\n"), tags)
         self.output_panel.configure(state="disabled")
+        self.scroll_output()
+
+    def scroll_output(self):
+        self.output_panel.see(tk.END)
 
     def create_widgets(self):
-        output = tk.Text(self.master, state=tk.DISABLED, name="output")
+        scrollbar = tk.Scrollbar(self.master)
+        scrollbar.grid(row=0, column=1, sticky=tk.N+tk.S)
+
+        output = tk.Text(self.master, state=tk.DISABLED, name="output", yscrollcommand=scrollbar.set)
+        scrollbar.config(command=output.yview())
+        output.scrollbar = scrollbar
         output.grid(row=0)
 
         input_area = tk.Entry(self.master, name="input")
@@ -44,8 +55,9 @@ class ClientUI(tk.Frame):
         text = user_input.widget.get()
         user_input.widget.delete(0, tk.END)
         self.send_command(text)
-        if True:
+        if self.client.config['UI'].getboolean('echo_input'):
             self.draw_output(text, 'italic')
+        self.scroll_output()
 
     def show_preferences(self):
         prefs = Preferences(self.master)
--- preferences/preferences.py
+++ preferences/preferences.py
@@ -1,5 +1,7 @@
 __author__ = 'ToothlessRebel'
-from tkinter import Frame, Toplevel
+from tkinter import Frame, Toplevel, Checkbutton, IntVar
+from configparser import ConfigParser
+from pprint import pprint
 
 
 class Preferences(Frame):
@@ -7,3 +9,12 @@ class Preferences(Frame):
         super(Preferences, self).__init__()
         prefs = Toplevel(self)
         prefs.wm_title("Preferences")
+
+        self.parser = ConfigParser()
+        if self.parser.read('config.ini').__len__() < 1:
+            raise EnvironmentError
+        # master.grid()
+        var = IntVar()
+
+        c = Checkbutton(master, text="Expand", variable=var)
+        c.pack()
