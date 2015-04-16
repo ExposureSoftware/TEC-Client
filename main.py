066fa88164bce84f9fc829e7c6cc166c63115c5e
Marshall Davis <toothlessrebel@gmail.com>
2015-04-16 01:14:55 -0400

    Finally adds installer.

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ from pkg_resources import resource_filename
 root = Tk()
 root.wm_title("The Eternal City")
 root.wm_iconbitmap(root, resource_filename('resources.images', 'eternal_logo.ico'))
-root.wm_iconbitmap()
 client = Client(root)
 root.mainloop()
--- setup.py
+++ setup.py
@@ -1,9 +1,36 @@
 __author__ = 'ToothlessRebel'
-from setuptools import setup, find_packages
+import sys
+from setuptools import find_packages
+from pkg_resources import resource_filename
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "include_files": [
+        ('resources', 'resources'),
+        ('config.ini', 'config.ini')
+    ]
+}
+
+base = None
+# if sys.platform == "win32":
+#     base = "Win32GUI"
 
 setup(
-    name='client',
-    version='0.1',
+    name="TEC Client",
+    version="0.3-alpha",
+    description="TEC Client in Python",
+    options={
+        "build_exe": build_exe_options
+    },
     packages=find_packages(),
-    package_data={'': ['*.png', '*.gif', '*.ico']}
+    package_data={'': ['*.png', '*.gif', '*.ico']},
+    executables=[
+        Executable(
+            "main.py",
+            base=base,
+            icon=resource_filename('resources.images', 'eternal_logo.ico'),
+            targetName="TEC Client.exe",
+            packages=find_packages()
+        )
+    ]
 )
