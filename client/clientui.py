bc3aa446b43945afaffbd382c02d6fb7993c3a87
PatrickDattilio <patrick.dattilio@gmail.com>
2015-10-10 22:07:02 -0400

    Potential fix for on/off crashes.

--- client/clientui.py
+++ client/clientui.py
@@ -41,7 +41,8 @@ class ClientUI(tk.Frame):
         # pprint(vars(master))
         self.side_bar = master.children['side_bar']
         self.output_panel = master.children['output_frame'].children['output']
-        # self.output_panel = self.output_frame.children['output']
+        self.output_panel.configure(state="normal")
+        self.output_panel.bind('<Key>',lambda e: 'break')
         self.input = master.children['input']
 
         self.char_width = Font(self.output_panel, self.output_panel.cget("font")).measure('0')
@@ -151,10 +152,7 @@ class ClientUI(tk.Frame):
                 pprint(skoot)
 
     def draw_output(self, text, tags=None):
-        self.output_panel.configure(state="normal")
-        # scroll_position = self.output_panel.scrollbar.get()
         self.output_panel.insert(tk.END, text, tags)
-        self.output_panel.configure(state="disabled")
         self.scroll_output()
 
         # If we're logging the session, we need to handle that
