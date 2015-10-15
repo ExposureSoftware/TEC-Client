d7c8315342dffb3c77e008b949cdedf3c3b30460
Marshall Davis <toothlessrebel@gmail.com>
2015-10-15 04:18:37 -0400

    Increments version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ base = None
 
 setup(
     name="Centurion Client",
-    version="0.5.3-alpha",
+    version="0.6-alpha",
     description="TEC Client in Python",
     options={
         "build_exe": build_exe_options
